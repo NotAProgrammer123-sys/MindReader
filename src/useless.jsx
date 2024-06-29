66c2db3777bce19fce821b213f39d3a6ef3d5a28
NotAprogrammer <154241349+NotAProgrammer123-sys@users.noreply.github.com>
2024-06-29 22:07:45 +0200

    New update!

---
+++ src/useless.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>Hello, THIS IS ABSOLUTELY FUC----- USELESS!!!! F--- COMMUN-</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
+//fix my life pls
